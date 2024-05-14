9f461ff76e5524615f8141422689d3713fe604ec
mslarson94 <83426018+mslarson94@users.noreply.github.com>
2024-05-13 20:15:24 -0700

    adding getTheoVector.py, renaming coinLoc_generator to dataConfigs, and adding dijkstras_sample files

--- dataConfigs.py
+++ dataConfigs.py
@@ -1,13 +1,29 @@
 '''
-CoinLocations_generator.py
+dataConfigs.py
 Created on March, 14 2024
 @author: myra
 
 generate CoinLocations.csv files from given x,y,z coordinates
 '''
 ########### Files & Coin Set ###### 
-whichCoinSet = 'A' # Possible Values (str): A, B, C, D
-outFile_pre = '~/Desktop/task_test_super/CoinLocations'
+whichCoinSet = 'B' # Possible Values (str): A, B, C, D
+outPath = '/Users/mairahmac/Desktop/task_test'
+outFile_pre = 'CoinLocations'
+
+
+############# Start Positions ###########
+# pos_ = [AN, PO]
+pos1 = [[0.0, 5.0],     [1.75, 4.25]] 
+pos2 = [[3.5, 3.5],     [4.25, 1.75]]
+pos3 = [[5.0, 0.0],     [4.25, -1.75]]
+pos4 = [[3.5, -3.5],    [1.75, -4.25]]
+pos5 = [[0.0, -5.0],    [-1.75, -4.25]]
+pos6 = [[-3.5, -3.5],   [-4.25, -1.75]]
+pos7 = [[-5.0, 0.0],    [-4.25, 1.75]]
+pos8 = [[-3.5, 3.5],    [-1.75, 4.25]]
+
+tutorial_pos = [[5, 1], [5, -1]]
+
 
 ############# Tutorial Coins ############
 tutorial_1 = [-2.0, -6.0, 0.0]
@@ -28,7 +44,8 @@ if whichCoinSet == 'A':
 
         #Null Value
         NV_1 = [0.75, 3.0, 0.0]
-        NV_2 = [0.2, -0.3, 0.0]
+        NV_2 = [0.2, -1.3, 0.0]
+
         collectionOrder_List = [HV_2, NV_1, LV_2, NV_2, LV_1, HV_1]
 
 elif whichCoinSet == 'B':
@@ -41,11 +58,12 @@ elif whichCoinSet == 'B':
 
         #Low Value
         LV_1 = [-0.4, 3.1, 2.0]
-        LV_2 = [3.0, -1.5, 2.0]
+        LV_2 = [0.5, -0.5, 2.0]
+
 
         #Null Value
         NV_1 = [-2.7, 1.3, 0.0]
-        NV_2 = [0.5, -0.5, 0.0]
+        NV_2 = [3.0, -1.5, 0.0]
         collectionOrder_List = [HV_2, NV_1, LV_2, NV_2, LV_1, HV_1]
 
 elif whichCoinSet == 'C':
---
+++ getTheoVector.py
@@ -0,0 +1,60 @@
+'''
+getTheoVector.py
+Created on May 13 2024
+@author: myra
+
+function to get the closest theorhetical position of either coins or starting positions 
+to an actual position produced from Reward Collectors Task output data
+'''
+
+import math
+from dataConfigs import *
+
+def getTheoVector(p, WhichTheo, droppedY=False):
+	'''
+	Takes actual coordinates supplied from Magic Leap 2 Reward Collectors Task (RC) output 
+	and finds the closest theoretical vector - be it coin position or starting position. 
+	
+	p = List of Floats. Actual x,y,z coordinates from the HeadPosDelta Column in the RC output file 
+	
+	WhichTheo = String. Which type of theorhetical coordinates to compare p to? Options = 'startPos' or 'coin'
+	
+	droppedY = Boolean. Whether the y value has already been dropped in the actual coordinate provided, default value is False
+	'''
+	p = p if droppedY == True else p[:1] + p[2:]
+	validWhichTheo = ['coin', 'startPos']
+	try:
+		if WhichTheo not in validWhichTheo:
+			raise ValueError
+	except ValueError:
+		print("ERROR: WhichTheo must be either 'coin' or 'startPos', try again")
+
+	else:
+		#print('stuff worked')
+		if WhichTheo == 'startPos':
+			position_list = [pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, tutorial_pos]
+			AN_positions = list(list(zip(*position_list))[0])
+			PO_positions = list(list(zip(*position_list))[1])
+			positionPossibilities = AN_positions + PO_positions
+			whoStartList = ['AN']*9 + ['PO']*9
+		else:
+			positionPossibilities = [[item for i, item in enumerate(sublist) if i != 2] for sublist in collectionOrder_List]
+			coinValues = [sublist[2] for sublist in collectionOrder_List if len(sublist) > 2]
+
+		distances = [math.dist(p, q) for q in positionPossibilities]
+		closestPosition = distances.index(min(distances))
+		#print('distances', distances)
+		#print('*'*25)
+		#print('closestPosition', closestPosition, distances[closestPosition], positionPossibilities[closestPosition])
+		coinValue = ''
+		actualPosition = positionPossibilities[closestPosition]
+		if WhichTheo == 'coin':
+			coinValue = coinValues[closestPosition]
+		else:
+			whoStart = whoStartList[closestPosition]
+		#print(actualPosition, coinValue, whoStart)
+		return actualPosition, coinValue, whoStart
+
+
+# myCoordinate = [0, 0, 1]
+# getTheoVector(myCoordinate, 'startPos')
